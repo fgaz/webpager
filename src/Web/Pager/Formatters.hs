bdceb11b65ef5ef0fe2652c39fbef6268fdcf1d4
Francesco Gazzetta <francygazz@gmail.com>
2017-05-28 19:12:51 +0200

    some docs

--- src/Web/Pager/Formatters.hs
+++ src/Web/Pager/Formatters.hs
@@ -13,14 +13,28 @@ import Data.Semigroup ((<>))
 import Numeric (showHex)
 import Data.Hashable (hash)
 
+-- ^ A formatter turns a line into a list of HTML fragments.
+-- Each fragment will be wrapped in a <td> (you don't need
+-- to do it (and in fact you shouldn't)) and put into a <tr>
+-- together with the rest of the line.
 type Formatter = Text -> [Html ()]
 
+-- ^ Utility function, used internally in 'irc'.
+-- Creates a color attribute by hashing the contents of its
+-- argument.
+-- Useful for usernames and such.
 colorHash :: Text -> Attribute
 colorHash t = style_ ("color:#" <> T.take 6 (hash' t) <>";")
 
 hash' :: Text -> Text
 hash' = T.pack . flip showHex "" . abs . hash
 
+-- ^ Parse irc logs, in the format outputted by the ii client.
+--
+-- > "date time <user> message (maybe with spaces)
+--
+-- becomes
+-- [date, time, uniquely colored <user>, message (maybe with spaces)]
 irc :: Formatter
 irc txt = [dateHtml, hourHtml, nameHtml, messageHtml]
   where dateHtml = toHtml $ T.takeWhile (/=' ') txt
@@ -33,6 +47,7 @@ irc txt = [dateHtml, hourHtml, nameHtml, messageHtml]
         nameHtml = span_ [nameColor] $ toHtml name
         messageHtml = toHtml message
 
+-- ^ Separates the words in the line, one cell each.
 oneWordPerCell :: Formatter
 oneWordPerCell txt = toHtml <$> T.words txt
 
