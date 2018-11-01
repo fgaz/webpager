5412cd1e1095b66fcfec127a3ed62c2ce39b0510
Francesco Gazzetta <francygazz@gmail.com>
2018-11-01 12:36:40 +0100

    Add ircUnixTime

--- src/Web/Pager/Formatters.hs
+++ src/Web/Pager/Formatters.hs
@@ -4,6 +4,7 @@ module Web.Pager.Formatters
 ( Formatter
 , colorHash
 , irc
+, ircUnixTime
 , oneWordPerCell
 ) where
 
@@ -11,8 +12,10 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import Lucid
 import Data.Semigroup ((<>))
+import Data.Maybe (fromMaybe)
 import Numeric (showHex)
 import Data.Hashable (hash)
+import Data.Time
 
 -- | A formatter turns a line into a list of HTML fragments.
 -- Each fragment will be wrapped in a <td> (you don't need
@@ -47,6 +50,32 @@ irc txt = [dateHtml, hourHtml, nameHtml, messageHtml]
         nameColor = colorHash name
         nameHtml = span_ [nameColor] $ toHtml name
         messageHtml = toHtml message
+
+-- | Parse irc logs, in _another_ format outputted by the ii client.
+--
+-- > "unixtime <user> message (maybe with spaces)
+--
+-- becomes
+-- [date, time, uniquely colored <user>, message (maybe with spaces)]
+ircUnixTime :: Formatter
+ircUnixTime txt = [dateHtml, hourHtml, nameHtml, messageHtml]
+  where timestamp = T.takeWhile (/=' ') txt
+        utcTime = fromMaybe zeroTime $
+                    parseTimeM
+                      True
+                      defaultTimeLocale
+                      "%s"
+                      $ T.unpack timestamp
+        localTime = utcToLocalTime utc utcTime
+        dateHtml = toHtml $ showGregorian $ localDay localTime
+        hourHtml = toHtml $ show $ localTimeOfDay localTime
+        txt' = T.drop 1 $ T.dropWhile (/=' ') txt
+        name = T.takeWhile (/=' ') txt'
+        message = T.drop 1 $ T.dropWhile (/=' ') txt'
+        nameColor = colorHash name
+        nameHtml = span_ [nameColor] $ toHtml name
+        messageHtml = toHtml message
+        zeroTime = UTCTime (ModifiedJulianDay 0) 0
 
 -- | Separates the words in the line, one cell each.
 oneWordPerCell :: Formatter
